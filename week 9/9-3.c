cb12f8606a0fdeb1674a7d5db24ad5a3b3a16cdb
pakuland3 <pakuland_1@naver.com>
2024-10-23 19:24:09 +0900

    1023

---
+++ week 9/9-3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int getLine(char* line){
+    int ch;
+    int i=0;
+    while((ch=getchar())!='\n') line[i++]=ch;
+    line[i]='\0';
+    return i;
+}
+
+char input[100];
+
+int main(){
+    FILE *fp_src,*fp_dest;
+    if((fp_src=fopen("output.txt","r"))==NULL){
+        printf("error");
+        return 0;
+    }
+    if((fp_dest=fopen("output3.txt","w"))==NULL){
+        printf("error");
+        return 0;
+    }
+    while(!feof(fp_src)){
+        fscanf(fp_src,"%s\n",input);
+        fprintf(fp_dest,"%s\n",input);
+    }
+    fclose(fp_src);
+    fclose(fp_dest);
+    return 0;
+}

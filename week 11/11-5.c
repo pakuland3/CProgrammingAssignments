6980bbae85992af02ebe044ba0c1f40c9e3df305
pakuland3 <pakuland_1@naver.com>
2024-11-09 13:27:56 +0900

    1109

--- week 11/11-5.c
+++ week 11/11-5.c
@@ -5,63 +5,98 @@ typedef struct{
     int x,y;
 }POINT;
 
-int menu;
-POINT *points[100];
-int size[100];
-int count=0;
-
 int inputMenu(){
     int input;
-    printf("ë©”ë‰´ ì…ë ¥==========================\n");
-    printf("1. point ë§Œë“¤ê¸°\n");
-    printf("2. point ì¶œë ¥í•˜ê¸°\n");
-    printf("3. í™”ë©´ í´ë¦¬ì–´\n");
-    printf("-1. ì¢…ë£Œ\n");
+    printf("1. ¸¸µé±â, 2. Ãâ·ÂÇÏ±â, 3. Å¬¸®¾î, 4. ÆÄÀÏ ÀúÀå, 5. ÆÄÀÏ ·Îµå, -1. Á¾·á : ");
     scanf("%d",&input);
     return input;
 }
 
-POINT* makePoint(){
-    scanf("%d",&size[count]);
-    POINT *p=(POINT*)malloc(size[count]*sizeof(POINT));
-    for(int i=0;i<size[count];i++){
+POINT* makePoint(int *count, int *sizes){
+    scanf("%d",&sizes[*count]);
+    POINT *p=(POINT*)malloc(sizes[*count]*sizeof(POINT));
+    for(int i=0;i<sizes[*count];i++){
         p[i].x=rand()%100;
         p[i].y=rand()%100;
     }
-    count++;
-    printf("ì˜ ë§Œë“¤ì—ˆìŠµë‹ˆë‹¤.\n");
+    *count=*count+1;
+    printf("Àß ¸¸µé¾ú½À´Ï´Ù.\n");
     return p;
 }
 
-void printPoint(POINT *points[], int count, int size[]){
+void printPoint(POINT *points[], int count, int sizes[]){
+    printf("%d\n",sizes[0]);
     for(int i=0;i<count;i++){
-        for(int j=0;j<size[i];j++) printf("x:%d, y:%d\n",points[i][j].x,points[i][j].y);
+        for(int j=0;j<sizes[i];j++){
+            printf("x:%d, y:%d\n",(points[i]+j)->x,(points[i]+j)->y);
+        }
     }
 }
 
 void freePoints(POINT *points[], int count){
     for(int i=0;i<count;i++) free(points[i]);
-    printf("í”„ë¡œê·¸ë¨ì„ ì¢…ë£Œí•©ë‹ˆë‹¤.");
+    printf("ÇÁ·Î±×·¥À» Á¾·áÇÕ´Ï´Ù.");
 }
 
-int saveFile(){
-
+int saveFile(POINT *points[], int count, int *sizes){
+    FILE *fp;
+    if((fp=fopen("points.bin","wb"))==NULL) return 0;
+    for(int i=0;i<count;i++){
+        fwrite(&sizes[i],sizeof(int),1,fp);
+        fwrite(points[i],sizeof(POINT),sizes[i],fp);
+    }
+    int endoffile=0;
+    fwrite(&endoffile,sizeof(int),1,fp);
+    fclose(fp);
+    return 1;
 }
 
-int loadFile(){
-    
+int loadFile(POINT *points[], int *count, int *sizes){
+    FILE *fp;
+    if((fp=fopen("points.bin","rb"))==NULL) return 0;
+    fseek(fp,0,SEEK_END);
+    int fSize=ftell(fp);
+    // int fCount=fSize/sizeof(POINT);
+    rewind(fp);
+    int idx=0;
+    while(1){
+        fread(&sizes[idx],sizeof(int),1,fp);
+        if(sizes[idx]==0) break;
+        points[idx]=(POINT*)malloc(sizeof(POINT)*sizes[idx]);
+        fread(points[idx],sizeof(POINT),sizes[idx],fp);
+        idx++;
+    }
+    fclose(fp);
+    *count=idx;
+    return fSize;
 }
 
 int main(){
+    int menu;
+    POINT *points[100];
+    int sizes[100];
+    int count=0;
     do{
         menu=inputMenu();
         switch(menu){
-            case 1: points[count]=makePoint();
-            case 2: printPoint(points,count,size);
-            case 3: system("cls");
+            case 1:
+                points[count]=makePoint(&count,sizes);
+                break;
+            case 2:
+                printPoint(points,count,sizes);
+                break;
+            case 3:
+                system("cls");
+                break;
             case 4:
-            case 5: 
-            case -1: freePoints(points,count);
+                saveFile(points,count,sizes);
+                break;
+            case 5:
+                loadFile(points,&count,sizes);
+                break;
+            case -1:
+                freePoints(points,count);
+                break;
         }
     }
     while(menu!=-1);
